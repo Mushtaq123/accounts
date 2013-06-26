7b5d6da0fff6d87342d6a60c7c4674c0ee74db91
agarwalamit <amit.agarwal@flipkart.com>
2013-06-26 22:50:53 +0530

    first commit

---
+++ src/com/example/accounts/CustomComparator.java
@@ -0,0 +1,9 @@
+package com.example.accounts;
+import java.util.Comparator;
+
+public class CustomComparator implements Comparator<Details> {
+    @Override
+    public int compare(Details o1, Details o2) {
+        return o1.getName().compareTo(o2.getName());
+    }
+}
